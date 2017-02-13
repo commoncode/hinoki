ce9793dda35659f4784da5777d9dc8ea3845a417
veronica <veronica@common.io>
2017-02-13 16:45:43 +1100

    yay, our spreadsheet has been created with menu items!

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=250)
+    english_name = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    ingredients = models.CharField(max_length=250)
+    item_number = models.IntegerField()
+    is_vegetarian = models.BooleanField()
+    is_vegan = models.BooleanField()
+    is_glutenfree = models.BooleanField()
