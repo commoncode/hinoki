fffda3b061423b462d4a807e380f83f86379a542
veronica <veronica@common.io>
2017-02-13 18:18:03 +1100

    function to return item name and number plus admin function

--- orders/models.py
+++ orders/models.py
@@ -4,10 +4,13 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=250)
-    english_name = models.CharField(max_length=250)
+    english_name = models.CharField(max_length=250, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    ingredients = models.CharField(max_length=250)
+    ingredients = models.CharField(max_length=250, blank=True)
     item_number = models.IntegerField()
-    is_vegetarian = models.BooleanField()
-    is_vegan = models.BooleanField()
-    is_glutenfree = models.BooleanField()
+    is_vegetarian = models.BooleanField(default=False)
+    is_vegan = models.BooleanField(default=False)
+    is_glutenfree = models.BooleanField(default=False)
+
+    def __str__(self):
+         return str(self.item_number) + "-" + self.name
