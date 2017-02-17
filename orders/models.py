d0464c50bbde07ee5d5db9eb2612432653560c0b
David Sanders <dsanders@rapilabs.com>
2017-02-17 12:25:57 +1100

    Added a category field with choices that match those on the menu

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,21 @@
+# -*- coding=utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = [
+    ('nigiri', 'Nigiri Pack'),
+    ('hoso_maki', 'Hoso Maki Pack'),
+    ('deka_maki', 'Deka Maki Pack'),
+    ('nosé_maki', 'Nosé Maki Pack'),
+    ('sushi', 'Sushi Pack'),
+    ('platters', 'Platters'),
+    ('misc', 'Miscellaneous'),
+]
+
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=250)
+    category = models.CharField(max_length=250, choices=CATEGORY_CHOICES)
     english_name = models.CharField(max_length=250, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     ingredients = models.CharField(max_length=250, blank=True)
